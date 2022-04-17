ef7144c6ade55d5b97a37e18387f6273bb909390
Travis Lehl <lehl.travis@yahoo.com>
2022-04-17 09:55:15 -0500

    update questions and style sheet

---
+++ assets/jss/questions.js
@@ -0,0 +1,33 @@
+var jsQuestions = [
+    {
+      title: "All of these are commonly used data types EXCEPT:",
+      choices: ["Strings", "Booleans", "Buttons", "Numbers"],
+      answer: "Buttons"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "JavaScript can be devestatingly frustrating and exciting at the same time",
+      choices: ["True", "False"],
+      answer: "True"
+    },
+    {
+      title: "DOM is an abreviation for ____",
+      choices: ["Data Object Module", "Draw Over Main", "Document Object Model", "Data Open Map"],
+      answer: "Document Object Model"
+    },
+    {
+      title: "What value will be returned within this expression: 'expression1 && expression2'?",
+      choices: ["Returns true if both the expressions are true", "Returns true if either one of the expressions are true",
+                "Returns true if neither expression is true"],
+      answer: "Returns true if both the expressions are true"
+    },
+    {
+      title: "What do we use to store data in memory, as well as keep track of data for the whole application?",
+      choices: ["Function", "Variable", "Conditional Statements", "Bootstrap"],
+      answer: "Variable"
+    }
+  ];
